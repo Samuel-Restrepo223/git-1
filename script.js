caba12bdb5b48ab4a1846969e1381662ad95d1e1
Samuel Restrepo <SamuelRestrepo@gmail.com>
2025-04-29 17:47:58 -0500

    se crea el nuevo proyecto

---
+++ script.js
@@ -0,0 +1,22 @@
+'use strict';
+//DOM
+/*
+const $root = window.document.getElementById("root");
+const $p = window.document.createElement("p");
+
+$root.appendChild($p);
+$p.textContent = "Hola";
+$p.innerHTML = ' <strong>DEVS!</strong>';
+
+
+$p  .remove();
+
+$root.innerHTML = '<button>Click</button>';
+
+const $buttom = document.querySelector('button');
+
+
+$buttom.addEventListener("click", ()=>{
+    console.log ("El usuario hizo click en el boton");
+})
+*/
